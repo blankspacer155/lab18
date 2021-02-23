0bb555a754fed98c964499f5330adb2048b3c4e2
blankspacer155 <monnoi.92@gmail.com>
2021-02-23 15:52:56 +0700

    finished

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,39 @@
 #include<iostream>
+#include<algorithm>
+#include<cmath>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect r1,Rect r2){
+ 
+ double lx,ly;
+ double xr,xl,yu,yd;
+ xr=min(r1.x+r1.w,r2.x+r2.w);
+ xl=max(r1.x,r2.x);
+if(xr<=xl)
+{
+	return 0;
+}
+lx=abs(xr-xl);
 
 
+ yu=min(r1.y,r2.y);
+ yd=max(r1.y-r1.h,r2.y-r2.h);
+ if(yu<=yd)
+ {
+	 return 0;
+ }
+ ly=abs(yu-yd);
+return lx*ly;
 
+}
+
+int main()
+{Rect R1 = {1,1,5,5};
+Rect R2 = {7,2,3,3};	
+cout << overlap(R1,R2);
+	return 0;
 }
--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,29 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *r1,Rect *r2){
+ 
+ double lx,ly;
+ double xr,xl,yu,yd;
+ xr=min(r1->x+r1->w,r2->x+r2->w);
+ xl=max(r1->x,r2->x);
+if(xr<=xl)
+{
+	return 0;
+}
+lx=abs(xr-xl);
 
 
+ yu=min(r1->y,r2->y);
+ yd=max(r1->y-r1->h,r2->y-r2->h);
+ if(yu<=yd)
+ {
+	 return 0;
+ }
+ ly=abs(yu-yd);
+
+
+
+return lx*ly;
 
 }
